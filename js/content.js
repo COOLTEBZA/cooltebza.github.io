1410aaef28deffe5e81e0c4f85f89537fa7911a8
Tebogo <59425267+COOLTEBZA@users.noreply.github.com>
2022-05-20 01:15:11 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -18,28 +18,28 @@ var cNav = `
           </li>
           
         <li class="li npbtn2" >
-          <a href="javascript:void(0)" class="a dropbtn1" onclick="my1Function()">Help Sector</a>
+          <a href="javascript:void(0)" class="a dropbtn1" onclick="my1Function()">Entertainment</a>
             <div class="dropdown1-content" id="myDropdown1">
-             <a class="a" href="#">Link 1</a>
-              <a class="a" href="#">Link 2</a>
-              <a class="a" href="#">Link 3</a>
+             <a class="a" href="#">TV</a>
+              <a class="a" href="#">Radio</a>
+              <a class="a" href="#">News</a>
             </div>
           </li>
           
         <li class="li npbtn2" >
             <a href="javascript:void(0)" class="a dropbtn2" onclick="my2Function()">More</a>
             <div class="dropdown2-content" id="myDropdown2">
-             <a class="a" href="#">Link 1</a>
-              <a class="a" href="#">Link 2</a>
-              <a class="a" href="#">Link 3</a>
+             <a class="a" href="#">Places</a>
+              <a class="a" href="#">Pros & Cons</a>
+              <a class="a" href="#">Areas</a>
             </div>
           </li>
           <li style="float:right;" class="li npbtn2" >
 		      <a href="javascript:void(0)" class="a dropbtn4" onclick="my4Function()">Settings</a>
             <div  class="dropdown4-content" id="myDropdown4">
-             <a class="a" href="#">Link 1</a>
-              <a class="a" href="#">Link 2</a>
-              <a class="a" href="#">Link 3</a>
+             <a class="a" href="#">Theme</a>
+              <a class="a" href="#">Tone</a>
+              <a class="a" href="#">Account</a>
             </div>
 		  </li>
 		  
