2e5af357cb8c4000ced3dbebae76792e6874496a
Tebogo <59425267+COOLTEBZA@users.noreply.github.com>
2022-06-10 21:17:40 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,4 +1,3 @@
-
 var cNav = `
 <ul class="ul">
         <li class="li npbtn2" ><a class="logo" id="logo" href="https://cooltebza.github.io/"><img class="" src="${c_logo}"></a></li>
@@ -7,13 +6,33 @@ var cNav = `
         <li class="li npbtn2" ><a class="a" id="contact" onclick="c_contact()">Contact</a></li>
 		<li class="li npbtn2" ><a class="a" id="about" onclick="c_about()">About</a></li>
         <li class="li npbtn2" >
-          <a href="javascript:void(0)" class="a dropbtn" onclick="myFunction()">Information System</a>
+           <a href="javascript:void(0)" class="a dropbtn" onclick="myFunction()">Information Systems</a>
             <div class="dropdown-content" id="myDropdown">
-             <a class="a" href="#">Education Sector</a>
-              <a class="a"  id="dictionary" onclick="c_dictionary()">Dictionary Sector</a>
-              <a class="a" href="#">Cultural Sector</a>
-			  <a class="a" href="#">Traditional Sector</a>
-			  <a class="a" href="#">Social Sector</a>
+                <a class="a" href="#">Educational Sector</a>
+				<a class="a" href="#"> <b>Primary/R/Creche School</b> </a>
+				<a class="a" href="#"> Grade R </a>
+				<a class="a" href="#"> Grade 1 </a>
+             <a class="a" href="#"> Grade 2 </a>
+             <a class="a" href="#"> Grade 3/Standard 1</a>
+             <a class="a" href="#"> Grade 4/Standard 2</a>
+             <a class="a" href="#"> Grade 5/Standard 3</a>
+             <a class="a" href="#"> Grade 6/Standard 4</a>
+             <a class="a" href="#"> Grade 7/Standard 5</a>
+			 <a class="a" href="#"> <b>Secondary/High/Comprehensive School</b> </a>
+             <a class="a" href="#"> Grade 8/Standard 6</a>
+             <a class="a" href="#"> Grade 9/Standard 7</a>
+             <a class="a" href="#"> Grade 10/Standard 8</a>
+             <a class="a" href="#"> Grade 11/Standard 9</a>
+             <a class="a" href="#"> Grade 12/Standard 10</a>
+			 <a class="a" href="#"> <b>Tertiary Feild</b> </a>
+	     <a class="a" href="#"> College/Campus</a>
+             <a class="a" href="#"> University</a>
+                <a class="a"  id="dictionary" onclick="c_dictionary()">Information Sector</a>
+                <a class="a" href="#">Arts & Cultural Sector</a>
+		<a class="a" href="#">Traditional & Religious Sector</a>
+		<a class="a" href="#">Public & Private Sector</a>
+		<a class="a" href="#">Help & Social Sector</a>
+		<a class="a" href="#">Law & Justice Sector</a>
             </div>
           </li>
           
@@ -37,7 +56,20 @@ var cNav = `
           <li style="float:right;" class="li npbtn2" >
 		      <a href="javascript:void(0)" class="a dropbtn4" onclick="my4Function()">Settings</a>
             <div  class="dropdown4-content" id="myDropdown4">
-             <a class="a" href="#">Theme</a>
+             <a class="a" href="#">
+			 <div >Theme</div> 
+			 <div> <select id="3" onchange="keyColor()">
+ <option >--Color--</option> 
+ <option value="white">Light</option> 
+ <option value="black">Dark</option> 
+ </select>
+ <div ></div>
+ <select id="3" onchange="keyBgColor()">
+ <option >--Background Color--</option> 
+  <option value="white">Light</option> 
+ <option value="black">Dark</option>
+ </select></div>
+			 </a>
               <a class="a" href="#">Tone</a>
               <a class="a" href="#">Account</a>
             </div>
@@ -48,3 +80,25 @@ var cNav = `
 
 `;
 
+
+var cNav0 = `
+<ul class="ul">
+        <li class="li npbtn2" ><a class="logo" id="logo" href="https://cooltebza.github.io/"><img class="" src="${c_logo}"></a></li>
+        <li class="li npbtn2" ><a class="a active" id="home" href="https://cooltebza.github.io/">Home</a></li>
+        <li class="li npbtn2" >
+          <a href="javascript:void(0)" class="a dropbtn" onclick="myFunction()">More</a>
+            <div class="dropdown-content" id="myDropdown">
+              <a class="a" id="">Term of Use</a>
+              <a class="a" id="news">News</a>
+              <a class="a" id="about">About</a>
+			  <a class="a" id="contact">Contact</a>
+			  <a class="a" id="dictionary">Porfolio</a>
+            </div>
+          </li>
+          
+        
+		  
+        
+        </ul>
+
+`;
